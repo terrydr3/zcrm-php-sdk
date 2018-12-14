521eb938245724512931e579ed805e87070b02e9
terrydr3 <drew@austinwebproject.com>
2018-12-13 18:37:00 -0600

    Update ZohoOAuth.php
    Change configuration location so you can use your local file

--- src/com/zoho/oauth/client/ZohoOAuth.php
+++ src/com/zoho/oauth/client/ZohoOAuth.php
@@ -23,7 +23,7 @@ class ZohoOAuth
 	    {
 	        if($configuration == null)
 	        {
-	            $configPath=realpath(dirname(__FILE__)."/../../../../resources/oauth_configuration.properties");
+	            $configPath=realpath(dirname(__FILE__)."/../../../../../../../../zoho_oauth_configuration.properties");
 	            $filePointer=fopen($configPath,"r");
 	            self::$configProperties = ZohoOAuthUtil::getFileContentAsMap($filePointer);
 	            if($configFilePointer!=false)
